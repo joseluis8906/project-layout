597380dff7383188b741b08b1f387293efa7da96
Jose Caceres <j6scaes@gmail.com>
2024-07-23 17:44:53 -0500

    refactor: better request yaml structure.

--- scripts/mtx/reqs/req.sh
+++ scripts/mtx/reqs/req.sh
@@ -6,4 +6,4 @@ METHOD=$(cat $1 | yq '.method')
 HEADERS=$(cat $1 | yq '.headers')
 BODY=$(cat $1 | yq '.body' -o=json)
 
-grpcurl -import-path=../../protobuf -import-path ../../protobuf/$SERVICE -proto "$PROTO" -H "$HEADERS" -d "$BODY" -plaintext localhost:50051 $METHOD | jq 
+grpcurl -import-path=../../../protobuf -import-path ../../../protobuf/$SERVICE -proto "$PROTO" -H "$HEADERS" -d "$BODY" -plaintext localhost:50051 "$SERVICE.$METHOD" | jq 
